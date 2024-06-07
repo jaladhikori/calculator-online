7642583a3f14988d1889fb618e1137e7e4098221
Jaladhi Kori <jaladhikori26@gmail.com>
2024-06-07 13:02:31 +0800

    edit

--- config/database.php
+++ config/database.php
@@ -2,23 +2,16 @@
 
 session_start();
 
-$host = 'localhost';
-$db   = 'calculator_online';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'calculator_online';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+if($mysqli->connect_error) {
+  echo "koneksi database error";
+  die("error!");
 }
 
 function session_check($login=false) {
--- layout/navbar.php
+++ layout/navbar.php
@@ -1,13 +1,17 @@
 <!-- Navbar Section Starts -->
 <nav>
-    <ul class="navbar">
-      <li class="logo"><i class="bx bx-math"></i>Calc <span>Online</span></li>  
+  <ul class="navbar">
+    <li class="logo"><i class="bx bx-math"></i>Calc <span>Online</span></li>
+    <div class="menu">
       <li><a href="index.php"><i class="bx bx-calculator"></i>Calculator</a></li>
       <li><a href="unitconversion.php"><i class="bx bx-analyse"></i>Unit Conversion</a></li>
       <li><a href="financeCalculator.php"><i class='bx bx-money-withdraw'></i>Finance Calculator</a></li>
-      <li><a href="penerbit.php"><i class="bx bx-group"></i>User</a></li>
-      <li class="menu-right"><a href="logout.php">Logout</a></li>
-    </ul>
+    </div>
+    <div class="menu-right">
+      <li><a href="user.php"><i class="bx bx-group"></i></a></li>
+      <li><a href="logout.php"><i class="bx bx-log-out-circle"></i></li>
+    </div>
+  </ul>
   </div>
 </nav>
 <!-- Navbar Section End -->
